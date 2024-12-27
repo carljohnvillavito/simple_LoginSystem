49e38695bf7394391b274e3c4fd20527b27fdb7c
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-12-27 21:09:01 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ document.getElementById('usersButton').addEventListener('click', function(event)
     fetch('/admin/users')
         .then(response => response.json())
         .then(data => {
-          const userDataContainer = document.getElementById('userDataContainer');
+            const userDataContainer = document.getElementById('userDataContainer');
             userDataContainer.style.display = 'block'; // Make container visible
 
             const userDataDiv = document.getElementById('userData');
@@ -11,13 +11,32 @@ document.getElementById('usersButton').addEventListener('click', function(event)
 
             data.forEach(user => {
                 const userDiv = document.createElement('div');
+
+                const passwordContainer = document.createElement('div');
+                passwordContainer.classList.add('password-container');
+
+                const passwordField = document.createElement('div');
+                passwordField.classList.add('password-field');
+                passwordField.textContent = user.password;
+
+                const copyButton = document.createElement('button');
+                copyButton.textContent = 'Copy';
+                copyButton.classList.add('copy-button');
+                 copyButton.addEventListener('click', function() {
+                     navigator.clipboard.writeText(user.password)
+                         .then(() => {
+                             alert('Password copied to clipboard!');
+                         })
+                     .catch(error => console.error('Failed to copy password:', error));
+                     });
+                passwordContainer.appendChild(copyButton);
+                passwordContainer.appendChild(passwordField);
                 userDiv.innerHTML = `
-                     <p><strong>Username:</strong> ${user.username}</p>
-                     <p><strong>Password:</strong> ${user.password}</p>
-                     <hr>
-                 `;
-                 userDataDiv.appendChild(userDiv);
-             });
+                    <p><strong>Username:</strong> ${user.username}</p>
+                `;
+                userDiv.appendChild(passwordContainer)
+                userDataDiv.appendChild(userDiv);
+            });
         })
         .catch(error => console.error('Error getting users:', error));
 });

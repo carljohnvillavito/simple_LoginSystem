761f6b4f169676cdb4a962833432bdba8e4ca9a5
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-12-27 19:51:58 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,10 @@
-// Functions for the websites.
+ document.getElementById('usersButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    fetch('/admin/users')
+      .then(response => response.json())
+      .then(data => {
+          console.log("Users:", data);
+          alert("Check console for user data");
+        })
+      .catch(error => console.error('Error getting users:', error));
+});

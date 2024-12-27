81aed2ab2463543bbbcb0dd1239ee3e28444feed
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-12-27 21:45:20 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,44 +1,112 @@
-document.getElementById('usersButton').addEventListener('click', function(event) {
-            event.preventDefault();
-            fetch('/admin/users')
-                .then(response => response.json())
-                .then(data => {
-                    const userDataContainer = document.getElementById('userDataContainer');
-                    userDataContainer.style.display = 'block'; // Make container visible
+document.addEventListener('DOMContentLoaded', function() {
 
-                    const userDataDiv = document.getElementById('userData');
-                    userDataDiv.innerHTML = ''; // Clear previous data
+        const registerForm = document.getElementById('registerForm');
+        const successPopup = document.getElementById('successPopup');
+         const successMessage = document.getElementById('successMessage');
+        const closePopup = document.getElementById('closePopup');
+        const passwordFields = document.querySelectorAll("input[type='password']");
 
-                    data.forEach(user => {
-                        const userDiv = document.createElement('div');
+        if (registerForm) {
+           registerForm.addEventListener('submit', function(event) {
+             event.preventDefault();
+             const formData = new URLSearchParams(new FormData(registerForm)).toString();
 
-                        const passwordContainer = document.createElement('div');
-                        passwordContainer.classList.add('password-container');
-
-                        const passwordField = document.createElement('div');
-                        passwordField.classList.add('password-field');
-                        passwordField.textContent = user.password;
-
-                        const copyButton = document.createElement('button');
-                        copyButton.textContent = 'Copy';
-                        copyButton.classList.add('copy-button');
-                            copyButton.addEventListener('click', function() {
-                                navigator.clipboard.writeText(user.password)
-                                    .then(() => {
-                                        alert('Password copied to clipboard!');
-                                    })
-                                .catch(error => console.error('Failed to copy password:', error));
-                                });
-                        passwordContainer.appendChild(copyButton);
-                        passwordContainer.appendChild(passwordField);
-
-                        userDiv.innerHTML = `
-                            <p><strong>Full Name:</strong> ${user.fullName}</p>
-                            <p><strong>Username:</strong> ${user.username}</p>
-                        `;
-                        userDiv.appendChild(passwordContainer);
-                        userDataDiv.appendChild(userDiv);
-                    });
+             fetch('/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                 },
+                 body: formData,
+              })
+                .then(response => {
+                   if (response.ok) {
+                        return response.text();
+                   } else {
+                        throw new Error('Registration failed');
+                   }
                 })
-                .catch(error => console.error('Error getting users:', error));
-        });
+                .then(html => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
+                    const errorMessage = doc.querySelector('.error');
+                     if(errorMessage){
+                         successMessage.textContent = '';
+                         successPopup.style.display = 'block';
+                         successMessage.textContent = errorMessage.textContent;
+                     }
+                     else {
+                          const successText = doc.querySelector('#successMessage').textContent;
+                          successMessage.textContent = successText;
+                          successPopup.style.display = 'block';
+                              passwordFields.forEach(field => {
+                                field.value = '';
+                              });
+                               registerForm.reset();
+                     }
+
+                })
+                 .catch(error => {
+                    successMessage.textContent = 'An error occurred during registration.';
+                   successPopup.style.display = 'block';
+                    console.error(error);
+               });
+           });
+       }
+
+       closePopup.addEventListener('click', function() {
+             successPopup.style.display = 'none';
+       });
+ });
+
+ function togglePassword(inputId) {
+         const passwordInput = document.getElementById(inputId);
+        if (passwordInput.type === "password") {
+              passwordInput.type = "text";
+        } else {
+             passwordInput.type = "password";
+       }
+ }
+document.getElementById('usersButton').addEventListener('click', function(event) {
+        event.preventDefault();
+        fetch('/admin/users')
+            .then(response => response.json())
+            .then(data => {
+                const userDataContainer = document.getElementById('userDataContainer');
+                userDataContainer.style.display = 'block'; // Make container visible
+
+                const userDataDiv = document.getElementById('userData');
+                userDataDiv.innerHTML = ''; // Clear previous data
+
+                data.forEach(user => {
+                    const userDiv = document.createElement('div');
+
+                    const passwordContainer = document.createElement('div');
+                    passwordContainer.classList.add('password-container');
+
+                    const passwordField = document.createElement('div');
+                    passwordField.classList.add('password-field');
+                    passwordField.textContent = user.password;
+
+                    const copyButton = document.createElement('button');
+                    copyButton.textContent = 'Copy';
+                    copyButton.classList.add('copy-button');
+                        copyButton.addEventListener('click', function() {
+                            navigator.clipboard.writeText(user.password)
+                                .then(() => {
+                                    alert('Password copied to clipboard!');
+                                })
+                            .catch(error => console.error('Failed to copy password:', error));
+                            });
+                    passwordContainer.appendChild(copyButton);
+                    passwordContainer.appendChild(passwordField);
+
+                    userDiv.innerHTML = `
+                        <p><strong>Full Name:</strong> ${user.fullName}</p>
+                        <p><strong>Username:</strong> ${user.username}</p>
+                    `;
+                    userDiv.appendChild(passwordContainer);
+                    userDataDiv.appendChild(userDiv);
+                });
+            })
+            .catch(error => console.error('Error getting users:', error));
+    });
